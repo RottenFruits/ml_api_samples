7b23ce8d044317122315f5d4c0f41d7c3decf137
RottenFruits <s_ogawa@akarf.com>
2019-07-28 13:20:55 +0900

    ajax support

--- ml_api_python/src/app/app.py
+++ ml_api_python/src/app/app.py
@@ -1,8 +1,10 @@
 from sklearn.externals import joblib
 import flask
+from flask_cors import CORS
 import numpy as np
 
 app = flask.Flask(__name__)
+CORS(app)
 model = None
 
 def load_model():
@@ -16,13 +18,10 @@ def predict():
         "Content-Type": "application/json"
     }
 
-    if flask.request.method == "POST":
-        feature = np.array([flask.request.form["sepal_length"],
-        flask.request.form["sepal_width"],
-        flask.request.form["petal_length"],
-        flask.request.form["petal_width"]]).reshape((1, -1))
+    if flask.request.get_json().get("feature"):
+        feature = flask.request.get_json().get("feature")
+        feature = np.array(feature).astype(int).reshape((1, -1))
         response["prediction"] = model.predict(feature).tolist()
-        print(response["prediction"])
 
         if response["prediction"][0] == 0:
             response["prediction"] = 'setosa'
@@ -32,21 +31,6 @@ def predict():
             response["prediction"] = 'virginica'
         response["success"] = True
     return flask.jsonify(response)
-
-    #     if flask.request.get_json().get("feature"):
-
-    #         feature = flask.request.get_json().get("feature")
-    #         feature = np.array(feature).reshape((1, -1))
-    #         response["prediction"] = model.predict(feature).tolist()
-
-    #         if response["prediction"] == 0:
-    #             response["prediction"] = 'setosa'
-    #         elif response["prediction"] == 1:
-    #             response["prediction"] = 'versicolor'
-    #         else:
-    #             response["prediction"] = 'virginica'
-    #         response["success"] = True
-    # return flask.jsonify(response)
 
 
 if __name__ == "__main__":
